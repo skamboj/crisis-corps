bd8d08f276271e9da1d26e3a0570da6d8e3939d5
Alex Schoof <alex.schoof@gmail.com>
2010-06-06 13:09:20 -0400

    i need to stop breaking things...

--- fbInterface.py
+++ fbInterface.py
@@ -131,8 +131,10 @@ class TaskHandler(webapp.RequestHandler):
             task.name = task_name
             task.task_id = Task.all().count() + 1
             task.owning_org = Organization.all().filter('org_id=',org_id).get()
-            task.callback = "http://crisiscorpsapp.appspot.com/api/" + (task.owning_org.Key()) + "/" + task.task_id
-            self.response.out.write("{callback:\"" + task.callback +"\" /}")
+            if task.owning_org:
+                task.callback = "http://crisiscorpsapp.appspot.com/api/" + (task.owning_org.Key()) + "/" + task.task_id
+                self.response.out.write("{callback:\"" + task.callback +"\" /}")
+            else: self.response.out.write("{callback: }")
         else:
             task = results.get()
         if self.request.get('desc'):
