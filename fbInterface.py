d1ad1f67bc37e8d4075454ce0ee8ab0d76fa1541
Alex Schoof <alex.schoof@gmail.com>
2010-06-06 03:08:50 -0400

    Added orgs, test page for users

--- fbInterface.py
+++ fbInterface.py
@@ -9,57 +9,86 @@ class testRoot(webapp.RequestHandler):
 
 class fbUserHandler(webapp.RequestHandler):
     def get(self):
-        self.response.out.write("Get!\n")
         if self.request.get('fb_id'):
             fb_id = cgi.escape(self.request.get('fb_id'))
             results = fbUser.gql("WHERE fb_id = :1", int(fb_id))
         else:
             results = fbUser.all()
-        if results.get() ==None : self.response.out.write("NOTHING FOUND\n")
+        if results.get() ==None : self.response.out.write("{}")
         else:
             for result in results:
                 self.response.out.write(result.to_xml())
 
     def post(self):
         fb_id = cgi.escape(self.request.get('fb_id'))
-        results = fbUser.gql("WHERE fb_id = :1", fb_id)
+        results = fbUser.gql("WHERE fb_id = :1", int(fb_id))
         if results.get() == None:
             # Create a new user
-            newUser = fbUser()
-            newUser.fb_id = int(cgi.escape(self.request.get('fb_id')))
-            newUser.lname = cgi.escape(self.request.get('lname'))
-            newUser.fname = cgi.escape(self.request.get('fname'))
-            if self.request.get('email'):
-                newUser.email = cgi.escape(self.request.get('email'))
-            newUser.location = cgi.escape(self.request.get('location'))
-            if self.request.get('interest_locations'):
-                newUser.interest_locations = cgi.escape(self.request.get('interest_locations')).split(',')
-            if self.request.get('skills'):            
-                newUser.skills = cgi.escape(self.request.get('skills')).split(',')
-            if self.request.get('interests'):            
-                newUser.interests = cgi.escape(self.request.get('interests')).split(',')
-            newUser.contact_preferences = cgi.escape(self.request.get('contact_preferences'))
-            newUser.put()
+            users = fbUser()
         else:
             user = results.get()
-            user.fb_id = int(cgi.escape(self.request.get('fb_id')))
+        user.fb_id = int(cgi.escape(self.request.get('fb_id')))
+        if self.request.get('lname'):
             user.lname = cgi.escape(self.request.get('lname'))
+        if self.request.get('fname'):            
             user.fname = cgi.escape(self.request.get('fname'))
-            if self.request.get('email'):
-                user.email = cgi.escape(self.request.get('email'))
+        if self.request.get('email'):
+            user.email = cgi.escape(self.request.get('email'))
+        if self.request.get('location'):            
             user.location = cgi.escape(self.request.get('location'))
-            if self.request.get('interest_locations'):
-                user.interest_locations = cgi.escape(self.request.get('interest_locations')).split(',')
-            if self.request.get('skills'):            
-                user.skills = cgi.escape(self.request.get('skills')).split(',')
-            if self.request.get('interests'):            
-                user.interests = cgi.escape(self.request.get('interests')).split(',')
+        if self.request.get('interest_locations'):
+            user.interest_locations = cgi.escape(self.request.get('interest_locations')).split(',')
+        if self.request.get('skills'):            
+            user.skills = cgi.escape(self.request.get('skills')).split(',')
+        if self.request.get('interests'):            
+            user.interests = cgi.escape(self.request.get('interests')).split(',')
+        if self.request.get('contact_preferences'):            
             user.contact_preferences = cgi.escape(self.request.get('contact_preferences'))
-            user.put()            
+        db.put(user)            
+
+class OrgHandler(webapp.RequestHandler):
+    def get(self):
+        if self.request.get('org_name'):
+            org_name = cgi.escape(self.request.get('org_name'))
+            results = Organization.gql("WHERE org_name = :1", org_name)
+        else:
+            results = Organization.all()
+        if results.get() ==None : self.response.out.write("{}")
+        else:
+            for result in results:
+                self.response.out.write(result.to_xml())
+
+    def post(self):
+        org_name = cgi.escape(self.request.get('org_name'))
+        results = Organization.gql("WHERE org_name = :1", org_name)
+        if results.get() == None:
+            # Create a new user
+            org = Organization()
+            api_key = org.put()
+            org.api_key = api_key
+        else:
+            org = results.get()
+        org.org_name = cgi.escape(self.request.get('org_name'))
+        if self.request.get('url'):
+            org.url = cgi.escape(self.request.get('url'))
+        if self.request.get('rank'):
+            org.rank = int(cgi.escape(self.request.get('rank')))
+        if self.request.get('active'):            
+            org.active = cgi.escape(self.request.get('active'))
+        if self.request.get('fbaccount'):
+            org.fbaccount = cgi.escape(self.request.get('fbaccount'))
+        if self.request.get('badge_image_url'):            
+            org.skills = cgi.escape(self.request.get('badge_image_url'))
+        if self.request.get('admins'):            
+            org.interests = cgi.escape(self.request.get('admins')).split(',')
+        db.put(org)            
+
+
 
 app = webapp.WSGIApplication([
         ('/fb/', testRoot),
-        ('/fb/fbUser', fbUserHandler)
+        ('/fb/fbUser', fbUserHandler),
+        ('fb/Organization', OrgHandler)
         ], debug=True)
 
 def main():
