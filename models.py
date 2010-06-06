cbf90d01398d031cfe606d9df2780bf93c6738bd
Montana Harkin <harking@Macky.local>
2010-06-06 11:49:53 -0400

    working organization signup. more styles

--- models.py
+++ models.py
@@ -15,10 +15,13 @@ class Organization(db.Model):
     org_name = db.StringProperty()
     org_id = db.IntegerProperty()
     url = db.LinkProperty()
+    application_url = db.LinkProperty()
     api_key = db.StringProperty()
     rank = db.IntegerProperty()
     active = db.BooleanProperty()
-    fbaccount = db.StringProperty()
+    #now pointing to facebook page as they might not know their account
+    #fbaccount = db.StringProperty()
+    fbaccount = db.LinkProperty()
     badge_image_url = db.StringProperty()
     admins = db.ListProperty(db.Email)
 
@@ -35,7 +38,7 @@ class Task(db.Model):
 class User_Task(db.Model):
     fb_id = db.IntegerProperty()
     fbUser = db.ReferenceProperty(fbUser)
-    task_id = IntegerProperty()
+    task_id = db.IntegerProperty()
     Task = db.ReferenceProperty(Task)
     Score = db.IntegerProperty()
     Status = db.IntegerProperty()
